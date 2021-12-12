340239aa6986652e14ca4f9fc604c25aeaf759b2
Rudy.88 <90784502+wRudyw88@users.noreply.github.com>
2021-12-12 19:43:54 +0100

    Correction of Login

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieSession = require('cookie-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authsRouter = require('./routes/auth')
 
 var app = express();
+let expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1h;
+app.use(
+  cookieSession({
+    name: "user",
+    keys: ["689HiHoveryDi79*"],
+    cookie: {
+      httpOnly: true,
+      expires: expiryDate,
+    },
+  })
+)
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auths', authsRouter);
 
 module.exports = app;
+

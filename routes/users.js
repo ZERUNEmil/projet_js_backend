5317ddb9229678208ff80ccb706113075085d152
Ciara VM <kieranvm@gmail.com>
2021-12-16 22:17:13 +0100

    Adding the UdpateProfil routes into userRoute

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,27 @@ router.get('/:email', async function(req, res){
       shadow_balance: user.shadow_balance,
       sales_number: user.sales_number,
       purchases_number: user.purchases_number,
-      profil_picture: user.profil_picture});
+      profil_picture: user.profil_picture,
+      email: user.email});
+});
+
+router.put('/:email/updateProfil', async function(req, res){
+  // Send an error code '400 Bad request' if the body parameters are not valid
+  if (
+    !req.body ||
+    (req.body.hasOwnProperty("email") && req.body.email.length === 0) ||
+    (req.body.hasOwnProperty("firstname") && req.body.firstname.length === 0) ||
+    (req.body.hasOwnProperty("lastname") && req.body.lastname.length === 0)
+  )
+    return res.status(400).end();
+
+  try {
+    let user = await userModel.updateProfil(req.params.email, req.body, req.app.pool);
+    if (!user) return res.status(304).end();
+    return res.json(user);
+  }catch (error){
+    return res.status(420).end();
+  }
 });
 
 module.exports = router;

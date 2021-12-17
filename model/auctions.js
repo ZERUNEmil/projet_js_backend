1d4fc9edbe7917a26687404f6e94593a3438ddf8
Onder0 <64706773+Onder0@users.noreply.github.com>
2021-12-17 19:17:34 +0100

    WIP Backend Auctions

--- model/auctions.js
+++ model/auctions.js
@@ -4,16 +4,35 @@
 class Auctions {
     constructor() {
     }
+    /**
+     * Returns all items
+     * @returns {Array} Array of items
+     */
+    async getAll(pool) {
+        const  { rows } = await pool.query('SELECT * FROM project.auction');
+        if (! rows) return;
+
+        return rows;
+    }
+
+    /**
+     * Returns the item identified by id
+     * @param {number} id - id of the item to find
+     * @returns {object} the item found or undefined if the id does not lead to a item
+     */
+    async getOne(id, pool) {
+        const  { rows } = await pool.query('SELECT * FROM project.auction WHERE id_auction = $1', [id]);
+        if (! rows) return;
+
+        return rows[0];
+    }
 
     /**
      * Add a item in the DB and returns - as Promise - the added item (containing a new id)
      * @param {object} body - it contains all required data to create a item
      * @returns {Promise} Promise reprensents the item that was created (with id)
      */
-    async addOne(body, pool) {
-
-
-
+    async addAuction(body, pool) {
     }
 
 }
--- routes/auctions.js
+++ routes/auctions.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const {Auctions} = require("../model/auctions");
 const auctionModel = new Auctions();
 
-
 /* GET auctions listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
@@ -18,7 +17,7 @@ router.get('/:idAuction', async function (req, res) {
     )
         return res.status(404).end();
 
-    const auction = await auctionModel.getOnel(req.params.idAuction, req.app.pool);
+    const auction = await auctionModel.getOne(req.params.idAuction, req.app.pool);
 
     return res.json({
         name: auction.name,
@@ -31,3 +30,35 @@ router.get('/:idAuction', async function (req, res) {
         cover_photo: auction.cover_photo,
     });
 })
+
+/* ADD ONE */
+router.put('/addAuction', async function (req, res) {
+    // Send an error code '400 Bad request' if the body parameters are not valid
+
+    // TODO faut récup l'id de l'utilisateur -> id owner
+
+    if (
+        !req.body ||
+        (req.body.hasOwnProperty("name") && req.body.name.length === 0) ||
+        (req.body.hasOwnProperty("description") && req.body.description.length === 0) ||
+        (req.body.hasOwnProperty("start_price") && req.body.start_price.length === 0) ||
+        (req.body.hasOwnProperty("day_duration") && req.body.day_duration.length === 0) ||
+        (req.body.hasOwnProperty("start_time") && req.body.start_time.length === 0) ||
+        (req.body.hasOwnProperty("cover_photo") && req.body.cover_photo.length === 0)
+    )
+        return res.status(400).end();
+
+    const auction = await auctionModel.addAuction(req.body, req.app.pool);
+
+    return res.json({
+        id_auction: auction.id_auction,
+        name: auction.name,
+        description: auction.description,
+        star_price: auction.star_price,
+        day_duration: auction.day_duration,
+        start_time: auction.start_time,
+        status: auction.status,
+        owner: auction.owner,
+        cover_photo: auction.cover_photo,
+    });
+});

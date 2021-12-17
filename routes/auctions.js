d9f5f38e627f6c2060148ee598218d7bc94c3f39
Onder0 <64706773+Onder0@users.noreply.github.com>
2021-12-17 18:22:59 +0100

    Update auctions.js

--- routes/auctions.js
+++ routes/auctions.js
@@ -1,22 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const { Auctions } = require("../model/auctions");
+const {Auctions} = require("../model/auctions");
 const auctionModel = new Auctions();
 
 
 /* GET auctions listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
 /* GET ONE */
-router.get('/FUNCTION', async function(req, res){
+router.get('/:idAuction', async function (req, res) {
     if (
         !req.body ||
-        (req.body.hasOwnProperty("id") && req.body.PARAM.length === 0)
+        (req.body.hasOwnProperty("idAuction") && req.body.idAuction.length === 0)
         // Vient des paramètre envoyés en json par le frontend
     )
         return res.status(404).end();
 
-    const auction = await
+    const auction = await auctionModel.getOnel(req.params.idAuction, req.app.pool);
+
+    return res.json({
+        name: auction.name,
+        description: auction.description,
+        star_price: auction.star_price,
+        day_duration: auction.day_duration,
+        start_time: auction.start_time,
+        status: auction.status,
+        owner: auction.owner,
+        cover_photo: auction.cover_photo,
+    });
 })
